8e8699814dcac976d083513c62641d41f3bb0f81
Luis <sifuncion@gmail.com>
2026-03-30 19:58:25 -0400

    pref: corrección de redirección en creación de artículos

--- app/Filament/Resources/RawArticleResource/Pages/CreateRawArticle.php
+++ app/Filament/Resources/RawArticleResource/Pages/CreateRawArticle.php
@@ -9,4 +9,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateRawArticle extends CreateRecord
 {
     protected static string $resource = RawArticleResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
