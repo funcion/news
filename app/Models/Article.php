675c3f26c18f3bf2e593be9e4b44d8b3b7f7d1a4
Luis <sifuncion@gmail.com>
2026-03-30 12:06:52 -0400

    feat: implementar interfaz HasMedia para soporte de imágenes

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Article extends Model
+class Article extends Model implements HasMedia
 {
-    use HasFactory;
+    use HasFactory, InteractsWithMedia;
 
     /**
      * The attributes that are mass assignable.
