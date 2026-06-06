e94e13fee02d499924312a1f2eb6165f06d25378
Luis <sifuncion@gmail.com>
2026-06-06 01:37:41 -0400

    Use Google Sans and Google Sans Flex font globally for both headings and content

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -62,10 +62,10 @@
         <meta name="twitter:image" content="{{ $ogImage }}" />
     @endif
 
-    <!-- Premium Fonts (Outfit for Headings, Inter for Body) -->
+    <!-- Google Sans Font (SIL Open Font License) -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&family=Outfit:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Google+Sans+Flex:opsz,wght@6..144,100..1000&family=Google+Sans:ital,wght@0,300;0,400;0,500;0,700;0,800;0,900;1,300;1,400;1,500;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
     <!-- Scripts & Styles -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
