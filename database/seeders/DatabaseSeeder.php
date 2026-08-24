253c66ba93bdc9ffac8b7569c1dae1c438e7d418
Luis Figuera <sifuncion@gmail.com>
2026-08-23 20:44:43 -0400

    revert(sources): preserve user-defined max_age_days and frequency per source without global override

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -585,8 +585,7 @@ class DatabaseSeeder extends Seeder
         ];
 
         foreach ($rssSources as $sourceData) {
-            $sourceData['max_age_days'] = 7;
-            $sourceData['frequency'] = 30;
+            $sourceData['max_age_days'] = 1;
             $sourceData['trusted'] = true;
             Source::create($sourceData);
         }
