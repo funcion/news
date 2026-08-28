f2dabbc87cb6aca59fd331ecb7c1ed80b2a8b3a3
Luis Figuera <sifuncion@gmail.com>
2026-08-28 12:10:35 -0400

    fix: add trustProxies middleware for HTTPS behind Traefik/Coolify

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        // Trust all proxies (Coolify/Traefik handles HTTPS)
+        $middleware->trustProxies(at: '*');
+
         $middleware->web(append: [
             \App\Http\Middleware\LocaleMiddleware::class,
         ]);
